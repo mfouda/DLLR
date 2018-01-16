90f7eef4cdea85e0ab176c90fdba92513c28ad69
sustecher <hkuandrewzhang@gmail.com>
2018-01-16 20:48:18 +0800

    add reshape input
    using real part or imag part can recon image,
    rather than magnitude and phase

--- main_reshape_input.m
+++ main_reshape_input.m
@@ -1,32 +1,43 @@
-addpath(genpath('E:\Yilong DATA\ESPIRiT'));
-addpath(genpath('E:\Yilong DATA\raw\2016_Nov_brain'));
 clc
 clear
 close all;
+addpath(genpath('F:\code\DLLR'));
+addpath(genpath('F:\Yilong DATA\ESPIRiT'));
+addpath(genpath('F:\Yilong DATA\raw\2016_Nov_brain'));
+imagesavepath = 'F:\Yilong DATA\Results Image\'; 
+ranksavepath = 'F:\Yilong DATA\Results Res\';
 
-load brain_8ch
+load mask_all
 load GreData
 [sx,sy,Sn,Nc]=size(gre_kxkyzc);
-for slice_n=1
-      for mask_n =1     
+ncalib = 48;
+ksize = [6,6]; % ESPIRiT kernel-window-size
+lkcc=ncalib^2*Nc;
+input_matrx=zeros(Sn,4,lkcc);
+tic
+for slice_n=1:1:20
             gre_kxkyzc=double(gre_kxkyzc);
+            [sx,sy,Sn,Nc]=size(gre_kxkyzc);
             DATA = gre_kxkyzc(:,:,slice_n,:);
             DATA = squeeze(DATA);
             DATA = DATA/max(max(max(abs(ifft2c(DATA))))) + eps;
-            % DATA = permute(DATA,[2 1 3]);
-            switch mask_n
-               case 1
-                   mask = mask_nocalib_x2half;
-               case 2
-                   mask = mask_nocalib_x3;
-               case 3
-                   mask = mask_randm_x3;
-               case 4
-                   mask = mask_randm_x4;
-           end
-                 mask_i = (size(mask,1)-size(DATA,1))./2 ;
-                 mask = imcrop(mask,[mask_i,mask_i,size(DATA,1)-1,size(DATA,1)-1]);
-                 figure; imshow(mask);
-                 DATAc = DATA.* repmat(mask,[1,1,Nc]);
+            
+     for mask_n =1:1:4   
+            mask=mask_all(:,:,mask_n);
+            mask=squeeze(mask);
+            mask_i = (size(mask,1)-size(DATA,1))./2 ;
+            mask = imcrop(mask,[mask_i,mask_i,size(DATA,1)-1,size(DATA,1)-1]);
+            DATAc = DATA.* repmat(mask,[1,1,Nc]);
+            calibc = crop(DATAc,[ncalib,ncalib,Nc]);
+            calibc=reshape(calibc,[lkcc,1]);
+            input_matrx(slice_n,mask_n,:)=real(calibc);   
       end
 end
+toc
+%% 
+input_matrx=reshape(input_matrx,[80 lkcc]);
+k=input_matrx(10,:);
+k=reshape(k,[48,48,32]);
+I=sos(ifft2c(k));
+imshow(I)
+
