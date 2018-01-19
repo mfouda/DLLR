b561e3992ca53dc720d2c2bd6a3f8f5d04ea7e9a
sustecher <hkuandrewzhang@gmail.com>
2018-01-19 14:29:11 +0800

    add normalized

--- Normalize_I.m
+++ Normalize_I.m
@@ -1,6 +1,15 @@
-function OutImg = Normalize_I(InImg)  
-ymax=255;ymin=0;  
-xmax = max(max(InImg)); %求得InImg中的最大值  
-xmin = min(min(InImg)); %求得InImg中的最小值  
-OutImg = round((ymax-ymin)*(InImg-xmin)/(xmax-xmin) + ymin); %归一化并取整  
+function OutImg = Normalize_I(InImg) 
+[sm ss sb sp]=size(InImg);
+ymax=128; ymin=-127;  
+for  i=1:1:sm
+    for j=1:1:ss
+        for k=1:1:sb
+            pixel_v=squeeze(InImg(i,j,k,:));
+            xmax = max(max(pixel_v)); %求得InImg中的最大值  
+            xmin = min(min(pixel_v)); %求得InImg中的最小值  
+            OutImg = round((ymax-ymin)*(pixel_v-xmin)/(xmax-xmin) + ymin); %归一化并取整  
+        end
+
+    end
+end
 end 
